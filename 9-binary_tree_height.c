96dbfb3387446c52a62c25be4dc97391028a877f
mehdizit <zitounimehdi.it@gmail.com>
2021-03-09 22:03:52 +0100

    up

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+* binary_tree_height - height binary tree
+* @tree: root node of tree
+* Return: height of tree
+*
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+int left_height = 0, right_height = 0;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
